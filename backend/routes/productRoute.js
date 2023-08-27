6ced794c58aa8149abbd3ec761ce71900d3a40ee
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-27 17:24:18 +0530

    create mongoose schema and post for prodcut creation

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const {
+  getAllProducts,
+  createProduct,
+} = require("../controllers/productController");
 
 // Getting router functionalities from express server
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
 
 module.exports = router;
