684cf85d81295b17a11c3f6d66ebbcc15ac50d54
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-27 14:12:03 +0530

    created dummy product route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,6 +4,6 @@ const { getAllProducts } = require("../controllers/productController");
 // Getting router functionalities from express server
 const router = express.Router();
 
-router.route("/products").post(getAllProducts);
+router.route("/products").get(getAllProducts);
 
 module.exports = router;
