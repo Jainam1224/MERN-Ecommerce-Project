3a621602a9ecfe574e8114a71a9128d231ed3046
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-28 23:47:56 +0530

    uncaught exception handled for server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+// uncaught Exception => e.g. if we are not able to find any field i.e. just uncomment console.log(youtube) in this file
+process.on("uncaughtException", (err) => {
+  console.log("err=>", err.message);
+  console.log("Shutting down the server due to Uncaught Exception");
+  process.exit(1);
+});
+
 // config path
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -14,9 +21,11 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
+// console.log(youtube);
+
 // Unhandled Promise Rejection => e.g. server url (DB_URI) of config.env file is wrong
 process.on("unhandledRejection", (err) => {
-  console.log("err", err.message);
+  console.log("err=>", err.message);
   console.log("Shutting down the server due to unhandled promise rejection");
 
   server.close(() => {
