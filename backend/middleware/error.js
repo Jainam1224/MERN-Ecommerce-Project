02df5e29582dd2d5e9f8245eab9c7e25b0fe3a4b
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-29 00:05:34 +0530

    mongodb id error handling implemented

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,6 +1,14 @@
+const ErrorHandler = require("../utils/errorHandler");
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
+
+  // MongoDB error like => invalid id
+  if (err.name === "CastError") {
+    message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
 
   res.status(err.statusCode).json({
     success: false,
