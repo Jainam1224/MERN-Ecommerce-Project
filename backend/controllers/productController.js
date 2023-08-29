d15a74069d2e62713c9326066d1b8e6be75583e3
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-29 23:32:25 +0530

    fixing product routes import

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require("../models/productModel");
-const ErrorHandler = require("../utils/ErrorHandler");
+const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ApiFeatures = require("../utils/apiFeatures");
 
