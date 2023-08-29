0349c59aa5601212d5b338caf146f504c962b3bc
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-30 01:11:07 +0530

    logout user api done

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/userController");
 
 // Getting router functionalities from express server
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logout);
 
 module.exports = router;
