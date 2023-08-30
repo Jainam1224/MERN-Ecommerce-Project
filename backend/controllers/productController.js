cee7946ac1a5a09fc6c55fd50976c3d1063e2cdd
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-30 22:12:24 +0530

    create and update review of product done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -93,3 +93,49 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     message: "Product deleted successfully",
   });
 });
+
+// Create New Review or Update the review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  // This is used to check if we have already reviewed this product and are updating that review or creating new review
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString()) {
+        rev.rating = rating;
+        rev.comment = comment;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avg = 0;
+
+  // Overall rating of all the reviews
+  product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
