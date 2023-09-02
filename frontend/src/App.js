611c27ffc61a90b5db4425eecea478e8f37b9fb0
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-09-02 13:10:35 +0530

    header component ui done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
+import Header from "./component/layout/Header/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
 
-function App() {}
+function App() {
+  // loading font styles before the page gets load
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
 
 export default App;
