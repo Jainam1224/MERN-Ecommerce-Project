24dc5185ff5c0854a0d3e937b8f565eb141b186f
Jainam Sanghvi <jainam.sanghvi@BLRETV-QRKQ3XR9.local>
2023-09-03 15:59:41 +0530

    home page featured products done and added smooth scroll functionality

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from 'react'
 import {CgMouse} from "react-icons/cg"
 import "./Home.css"
+import ProductCard from "./ProductCard.js"
+
+const product = {
+  name: "Blue Tshirt",
+  images: [{url: "https://i.ibb.co/DRST11n/1.webp"}],
+  price: 999,
+  _id: "Jainam"
+}
 
 const Home = () => {
   return (
@@ -13,6 +21,19 @@ const Home = () => {
               Scroll <CgMouse />
             </button>
           </a>
+       </div>
+
+       <h2 className="homeHeading">Featured Products</h2>
+       
+       <div className='container' id='container'>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
+       <ProductCard product={product}/>
        </div>
     </Fragment>
   )
