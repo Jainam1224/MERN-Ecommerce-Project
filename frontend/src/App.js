b546a264d94c627e27cd5e5bba57786e43ce9f26
Jainam Sanghvi <jainam.sanghvi@BLRETV-QRKQ3XR9.local>
2023-09-03 14:29:36 +0530

    home page main image and button implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import { useEffect } from "react";
 import "./App.css";
 import Header from "./component/layout/Header/Header";
 import Footer from "./component/layout/Footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
+import Home from "./component/Home/Home"
 
 function App() {
   // loading font styles before the page gets load
@@ -16,8 +17,11 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router> 
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </Router>
   );
