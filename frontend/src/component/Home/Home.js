2834554c5bce6e2f2b6cf3b87e432696f535fdf1
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-09-09 20:18:38 +0530

    loader ui implemented

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -5,6 +5,7 @@ import ProductCard from "./ProductCard.js";
 import MetaData from "../layout/MetaData";
 import { getProduct } from "../../actions/productAction";
 import { useDispatch, useSelector } from "react-redux";
+import Loader from "../layout/Loader/Loader";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -19,25 +20,31 @@ const Home = () => {
 
   return (
     <Fragment>
-      {/* This is the title of google chrome tabs */}
-      <MetaData title={"ECOMMERCE"} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          {/* This is the title of google chrome tabs */}
+          <MetaData title={"ECOMMERCE"} />
 
-      <div className="banner">
-        <p>Welcome to Ecommerce</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
-        <a href="#container">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
-      </div>
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
 
-      <h2 className="homeHeading">Featured Products</h2>
+          <h2 className="homeHeading">Featured Products</h2>
 
-      <div className="container" id="container">
-        {products &&
-          products.map((product) => <ProductCard product={product} />)}
-      </div>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <ProductCard product={product} />)}
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
