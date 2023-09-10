122f8d664a3544ab2dec22aa0e9d9cc7e7816796
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-09-10 12:25:04 +0530

    headers tab -> product page -> all products are done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ import Footer from "./component/layout/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
 import Home from "./component/Home/Home";
-import ProductDetails from "./component/Product/ProductDetails.js";
+import ProductDetails from "./component/Product/ProductDetails";
+import Products from "./component/Product/Products";
 
 function App() {
   // loading font styles before the page gets load
@@ -23,6 +24,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/product/:id" element={<ProductDetails />} />
+        <Route exact path="/products" element={<Products />} />
       </Routes>
       <Footer />
     </Router>
---
+++ frontend/src/component/Product/Products.js
@@ -0,0 +1,59 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./Products.css";
+import { useSelector, useDispatch } from "react-redux";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import Loader from "../layout/Loader/Loader";
+import ProductCard from "../Home/ProductCard";
+import Slider from "@material-ui/core/Slider";
+import { useAlert } from "react-alert";
+import Typography from "@material-ui/core/Typography";
+import MetaData from "../layout/MetaData";
+
+const categories = [
+  "Laptop",
+  "Footwear",
+  "Bottom",
+  "Tops",
+  "Attire",
+  "Camera",
+  "SmartPhones",
+];
+
+const Products = () => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { error, loading, products, productsCount } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct());
+  }, [dispatch, alert, error]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="PRODUCTS -- ECOMMERCE" />
+          <h2 className="productsHeading">Products</h2>
+
+          <div className="products">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Products;
