c3c58add2e18caa6ff43a1591cccd18f2100593b
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-09-16 19:46:13 +0530

    upload profile image - backend logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -202,7 +202,25 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     email: req.body.email,
   };
 
-  // WE WILL ADD AVATAR UPDATION LATER AS WE NEED TO USE CLOUDINARY PACKAGE
+  // AVATAR UPDATION USING CLOUDINARY PACKAGE
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+
+    const imageId = user.avatar.public_id;
+
+    await cloudinary.v2.uploader.destroy(imageId);
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
 
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
