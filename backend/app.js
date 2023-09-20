99e55669de19a2cc57361ac4908c39504a5adf3b
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-09-20 15:40:30 +0530

    payments page and also test payment integrated using stripe - created controller and route for payments as well

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
 const errorMiddleware = require("./middleware/error");
+// This is used to give the configuration added inside config.env file
+const dotenv = require("dotenv");
+
+// config path
+dotenv.config({ path: "backend/config/config.env" });
 
 // This is used to read the json objects and this is also called a middleware
 app.use(express.json());
@@ -18,10 +23,12 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 // Middleware For Error
 app.use(errorMiddleware);
